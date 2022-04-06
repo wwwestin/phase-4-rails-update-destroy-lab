b3bbffee7eec3cd5f813ee8e35a5ba15cdcdfca9
wwwestin <westin.h@gmail.com>
2022-04-06 09:31:30 -0400

    Completed lab

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,19 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+  
   private
 
   def plant_params
